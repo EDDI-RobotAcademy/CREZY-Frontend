37713c354ef86fe0837faede5601236ddba53ced
Hakgyu Kim <zage4448@gmail.com>
2023-08-16 11:57:27 +0900

    init: [김학규] 초기 환경 구성

---
+++ src/utility/axiosInst.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+import env from "@/env";
+
+const springAxiosInst = axios.create({
+    baseURL: env.api.SPRING_API_URL,
+    timeout: 2500,
+})
+
+const fastApiAxiosInst = axios.create({
+    baseURL: env.api.FAST_API_URL,
+    timeout: 2500
+})
+
+export default { springAxiosInst, fastApiAxiosInst }
