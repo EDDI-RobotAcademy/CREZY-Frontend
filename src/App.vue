58c34388f19a02312721de3d8861fc128af953ef
Hakgyu Kim <zage4448@gmail.com>
2023-08-17 15:38:42 +0900

    feat: [김학규] 배경 영상 설정 [MUSE-V-3]

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
-  <v-app style="background-color: black">
-    <NavigationMenu />
-    <v-main>
+  <v-app class="app-container">
+    <VideoBackground
+      :src="require(`@/assets/vids/vid-background.mp4`)"
+      style="
+        height: 100vh;
+        width: 100%;
+        position: absolute;
+        z-index: -1;
+        background-size: cover;
+      "
+    >
+    </VideoBackground>
+    <NavigationMenu style="z-index: 1" />
+    <v-main class="main">
       <div class="centered">
         <router-view />
       </div>
@@ -11,10 +22,12 @@
 
 <script>
 import NavigationMenu from "./views/layout/navigation/NavigationMenu.vue";
+import VideoBackground from "vue-responsive-video-background-player";
 
 export default {
   components: {
     NavigationMenu,
+    VideoBackground,
   },
 
   data: () => ({}),
@@ -22,6 +35,24 @@ export default {
 </script>
 
 <style>
+.app-container {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+}
+.main {
+  padding-bottom: 150px;
+  z-index: 1;
+  overflow: auto;
+  height: 100%;
+
+  -ms-overflow-style: none; /* 인터넷 익스플로러 */
+  scrollbar-width: none; /* 파이어폭스 */
+}
+
+.main::-webkit-scrollbar {
+  display: none; /* 크롬, 사파리, 오페라, 엣지 */
+}
 .centered {
   display: flex;
   flex-direction: column;
