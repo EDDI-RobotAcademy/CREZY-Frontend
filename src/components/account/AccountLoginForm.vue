4fb5a03976924eb70edec878877f7adf3ef3158c
hanhwack <hanhwack93@gmail.com>
2023-08-23 17:05:59 +0900

    feat: [한확] 유저가 로그인 페이지로 이동이 가능해진다 [MUSE-V-7]

---
+++ src/components/account/AccountLoginForm.vue
@@ -0,0 +1,34 @@
+<template lang="">
+    <v-container>
+        <v-card class="login-card" flat>
+            <div class="login-title">로그인</div>
+            <v-img
+                  class="mx-auto"
+                  height="300"
+                  :src="require('@/assets/images/blackpeoplewithheadphone.png')"
+                >
+            </v-img>
+        </v-card>
+    </v-container>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+.login-card {
+  margin-top: 6rem;
+  padding: 18px;
+  width: 900px;
+  background-color: rgba(0, 0, 0, 0.4) !important;
+}
+
+.login-title {
+  font-size: 22px;
+  color: #ffffff;
+}
+    
+</style>
---
+++ src/router/account/index.js
@@ -0,0 +1,11 @@
+import AccountLoginPage from "@/views/account/AccountLoginPage.vue";
+
+const accountRoutes = [
+  {
+    path: "/account-login-page",
+    name: "AccountLoginPage",
+    component: AccountLoginPage,
+  },
+];
+
+export default accountRoutes;
