b892ff257660bd3469bf5f5bd29c2389052194cb
hanhwack <hanhwack93@gmail.com>
2023-08-25 16:26:51 +0900

    feat: [한확] 유저가 구글로 회원가입이 가능해진다 [MUSE-V-7]

---
+++ src/views/account/oauth/GoogleRedirectPage.vue
@@ -0,0 +1,30 @@
+<template>
+    <div></div>
+  </template>
+  
+  <script>
+  import {useRoute} from "vue-router";
+  import {onMounted} from "vue";
+  import {useStore} from "vuex";
+  export default {
+    setup() {
+      const store = useStore()
+      const requestOauthGoogleToSpring = (code)=> store.dispatch("AccountModule/requestOauthGoogleToSpring", code)
+      async function setRedirectData() {
+        const route = useRoute()
+        const code = route.query.code
+        await requestOauthGoogleToSpring(code)
+      }
+      onMounted(()=> {
+        setRedirectData()
+        }
+      )
+      return {
+        requestOauthGoogleToSpring
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  </style>
