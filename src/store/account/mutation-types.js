9a91d14832e520cf326ccc60e0735bb66127394a
hanhwack <hanhwack93@gmail.com>
2023-08-28 16:46:14 +0900

    fix: [한확] userToken user에 통합 [MUSE-V-8]

--- src/store/account/mutation-types.js
+++ src/store/account/mutation-types.js
@@ -1,2 +1,1 @@
-export const SET_ACCESS_TOKEN = "SET_ACCESS_TOKEN"
 export const SET_USER = "SET_USER"
--- src/store/account/mutations.js
+++ src/store/account/mutations.js
@@ -1,11 +1,8 @@
 import {
-    SET_ACCESS_TOKEN, SET_USER
+    SET_USER
 } from './mutation-types'
 
 export default {
-    [SET_ACCESS_TOKEN] (state, receiveData) {
-        state.accessToken = receiveData
-    },
     [SET_USER] (state, receiveData) {
         state.user = receiveData
     },
