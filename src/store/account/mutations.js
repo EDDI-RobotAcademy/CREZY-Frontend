4d1322ec507fc05e6ed8430c5ae0c18599455354
jinho1593 <50562662+jinho1593@users.noreply.github.com>
2023-08-31 19:32:09 +0900

    feat: [김진호] 사용자가 로그인 또는 로그아웃을 하면 내비게이션바가 알맞은 상태로 변경됩니다[MUSE-V-17]

--- src/store/account/mutations.js
+++ src/store/account/mutations.js
@@ -1,14 +1,16 @@
 import {
-
-    REQUEST_ACCOUNT_TO_SPRING, SET_ACCOUNT
+    REQUEST_ACCOUNT_TO_SPRING, SET_ACCOUNT, SET_LOGGED_IN
 } from './mutation-types'
 
 export default {
-    [SET_ACCOUNT] (state, receiveData) {
+    [SET_ACCOUNT](state, receiveData) {
         state.account = receiveData
     },
     [REQUEST_ACCOUNT_TO_SPRING](state, receivedData) {
         state.account = receivedData;
-  },
-   
+    },
+    [SET_LOGGED_IN](state, receiveData) {
+        state.isLoggedIn = receiveData;
+    }
+
 }
