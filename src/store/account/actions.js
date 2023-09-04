24f32c372e46c319f109e8e4a35637c5efb4edcb
hanhwack <hanhwack93@gmail.com>
2023-09-04 18:08:39 +0900

    update: [한확] 구글 로그인 시 닉네임과 프로필 이미지도 추가로 받음[MUSE-V-8]

--- src/store/account/actions.js
+++ src/store/account/actions.js
@@ -2,108 +2,125 @@
 import {
   REQUEST_ACCOUNT_TO_SPRING,
   SET_ACCOUNT,
-  SET_LOGGED_IN
+  SET_LOGGED_IN,
 } from "./mutation-types";
-import axiosInst from '@/utility/axiosInst'
+import axiosInst from "@/utility/axiosInst";
 
 export default {
   requestAccountInfoToSpring({ commit }) {
-    const userToken = localStorage.getItem("userToken")
+    const userToken = localStorage.getItem("userToken");
 
-    return axiosInst.springAxiosInst.get('account/info', { headers: { Authorization: userToken } })
+    return axiosInst.springAxiosInst
+      .get("account/info", { headers: { Authorization: userToken } })
       .then((res) => {
         commit(REQUEST_ACCOUNT_TO_SPRING, res.data);
-        console.log(res.data)
-      })
+        console.log(res.data);
+      });
   },
 
   async requestGoogleOauthRedirectUrlToSpring() {
-    return axiosInst.springAxiosInst.get('/oauth/google')
-      .then(res => {
-        window.location.href = res.data
-      })
+    return axiosInst.springAxiosInst.get("/oauth/google").then((res) => {
+      window.location.href = res.data;
+    });
   },
-  async requestUserInfoGoogleToSpring(context, code) {
-    return axiosInst.springAxiosInst.get("/oauth/google-login", { params: { code: code } })
-      .then(async (res) => {
-        await context.commit(SET_ACCOUNT, res.data)
-        localStorage.setItem("userToken", res.data.userToken)
 
+  async requestUserInfoGoogleToSpring(context, payload) {
+    const { code, nickname, profileImage } = payload;
+
+    const reqForm = {
+      nickname,
+      profileImage,
+    };
+
+    return axiosInst.springAxiosInst
+      .post("/oauth/google-login", reqForm, { params: { code: code } })
+      .then(async (res) => {
+        await context.commit(SET_ACCOUNT, res.data);
+        localStorage.setItem("userToken", res.data.userToken);
         await context.commit(SET_LOGGED_IN, true);
-      })
+      });
   },
 
   async requestUserLogoutToSpring(context, code) {
-    const userToken = localStorage.getItem("userToken")
-    return axiosInst.springAxiosInst.get("/account/logout", { params: { userToken: userToken } })
+    const userToken = localStorage.getItem("userToken");
+    return axiosInst.springAxiosInst
+      .get("/account/logout", { params: { userToken: userToken } })
       .then(async (res) => {
         await context.commit(SET_LOGGED_IN, false);
-      })
+      });
   },
 
   async requestKakaoOauthRedirectUrlToSpring() {
-    return axiosInst.springAxiosInst.get('/oauth/kakao')
-      .then(res => {
-        window.location.href = res.data
-      })
+    return axiosInst.springAxiosInst.get("/oauth/kakao").then((res) => {
+      window.location.href = res.data;
+    });
   },
   async requestUserInfoKakaoToSpring(context, code) {
-    return axiosInst.springAxiosInst.get("/oauth/kakao-login", { params: { code: code } })
+    return axiosInst.springAxiosInst
+      .get("/oauth/kakao-login", { params: { code: code } })
       .then(async (res) => {
-        await context.commit(SET_ACCOUNT, res.data)
-        localStorage.setItem("userToken", res.data.userToken)
+        await context.commit(SET_ACCOUNT, res.data);
+        localStorage.setItem("userToken", res.data.userToken);
 
         await context.commit(SET_LOGGED_IN, true);
-
-      })
+      });
   },
 
   async requestNaverOauthRedirectUrlToSpring() {
-    return axiosInst.springAxiosInst.get('/oauth/naver')
-      .then(res => {
-        window.location.href = res.data
-      })
+    return axiosInst.springAxiosInst.get("/oauth/naver").then((res) => {
+      window.location.href = res.data;
+    });
   },
   async requestUserInfoNaverToSpring(context, code) {
-    return axiosInst.springAxiosInst.get("/oauth/naver-login", { params: { code: code } })
+    return axiosInst.springAxiosInst
+      .get("/oauth/naver-login", { params: { code: code } })
       .then(async (res) => {
-        await context.commit(SET_ACCOUNT, res.data)
-        localStorage.setItem("userToken", res.data.userToken)
+        await context.commit(SET_ACCOUNT, res.data);
+        localStorage.setItem("userToken", res.data.userToken);
 
         await context.commit(SET_LOGGED_IN, true);
-
-      })
+      });
   },
 
-  requestCheckNicknameToSpring({ }, payload) {
-    const { newNickname } = payload
-    return axiosInst.springAxiosInst.get(`/account/check-nickName/${newNickname}`)
+  requestCheckNicknameToSpring({}, payload) {
+    const { newNickname } = payload;
+    return axiosInst.springAxiosInst
+      .get(`/account/check-nickName/${newNickname}`)
       .then((res) => {
         if (res.data) {
-          alert('사용 가능한 닉네임입니다!')
-          return true
+          alert("사용 가능한 닉네임입니다!");
+          return true;
         } else {
-          alert('중복된 닉네임입니다!')
-          return false
+          alert("중복된 닉네임입니다!");
+          return false;
         }
-      })
+      });
   },
-  async requestChangeNicknameToSpring({ }, payload) {
-    const userToken = localStorage.getItem('userToken');
-    const { newNickname } = payload
+  async requestChangeNicknameToSpring({}, payload) {
+    const userToken = localStorage.getItem("userToken");
+    const { newNickname } = payload;
 
-    return axiosInst.springAxiosInst.get(`/account/change-nickname?userToken=${userToken}&nickname=${newNickname}`)
+    return axiosInst.springAxiosInst
+      .get(
+        `/account/change-nickname?userToken=${userToken}&nickname=${newNickname}`
+      )
       .then((res) => {
         console.log(res.data);
-      })
+      });
   },
-  async requestChangeProfileImageToSpring({ }, payload) {
-    const userToken = localStorage.getItem('userToken');
-    const { newProfileImageName } = payload
 
-    return axiosInst.springAxiosInst.get('/account/change-profileImage', { headers: { Authorization: userToken }, params: { profileImageName: newProfileImageName } })
+  async requestChangeProfileImageToSpring({}, payload) {
+    const userToken = localStorage.getItem("userToken");
+    const { newProfileImageName } = payload;
+
+    return axiosInst.springAxiosInst
+      .get("/account/change-profileImage", {
+        headers: { Authorization: userToken },
+        params: { profileImageName: newProfileImageName },
+      })
       .then((res) => {
         console.log(res.data);
-      })
+      });
   },
-}
+  
+};
