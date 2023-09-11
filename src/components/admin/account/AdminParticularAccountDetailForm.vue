15b2a426f8e22af9a5b94a9f5ecd68885f8a8cbf
Hakgyu Kim <zage4448@gmail.com>
2023-09-11 11:19:35 +0900

    feat: [김학규] 관리자는 등록 된 계정들 전체의 정보를 간략하게 확인 할 수 있다 중간 세이브 [MUSE-V-35]

---
+++ src/components/admin/account/AdminParticularAccountDetailForm.vue
@@ -0,0 +1,18 @@
+<template >
+    <div>
+        {{ accountId }}
+    </div>
+</template>
+<script>
+export default {
+  props: {
+    accountId: {
+        type: Number,
+        required: true
+    }
+  }
+}
+</script>
+<style lang="">
+    
+</style>
