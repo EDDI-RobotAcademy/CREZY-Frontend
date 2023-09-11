14a25eb222bc033b468bbcb1d3e06b310b5f5c23
hanhwack <hanhwack93@gmail.com>
2023-09-11 17:49:46 +0900

    feat: [한확] 관리자 리포트 관리 틀 생성 [MUSE-V-37]

---
+++ src/views/admin/report/AdminOverallReportsPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+       <AdminOverallReportsForm/>   
+    </div>
+  </template>
+  
+  <script>
+  import AdminOverallReportsForm from "@/components/admin/report/AdminOverallReportsForm.vue"
+  
+  export default {
+    components: {
+      AdminOverallReportsForm
+    }
+  }
+  </script>
+  
+  <style>
+      
+  </style>
