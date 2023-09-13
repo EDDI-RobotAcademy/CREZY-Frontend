6bd3c37a0461879e96dc6d3bc87ecb7be32b50f7
Hakgyu Kim <zage4448@gmail.com>
2023-09-13 17:28:11 +0900

    update: [김학규] 관리자 계정 관리 페이지 axios 연결 [MUSE-V-35]

---
+++ src/store/admin/account/states.js
@@ -0,0 +1,5 @@
+export default {
+    accountsStatus: {},
+    accountList: [],
+    accountListCount: 0
+  };
--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,7 @@ import AccountModule from "./account/AccountModule";
 import SongModule from "./song/SongModule"
 import ReportModule from "./report/ReportModule";
 import InquiryModule from "./inquiry/InquiryModule"
+import AdminAccountModule from "./admin/account/AdminAccountModule";
 
 import createPersistedState from 'vuex-persistedstate';
 
@@ -17,6 +18,7 @@ export default createStore({
     songModule: SongModule,
     reportModule: ReportModule,
     inquiryModule: InquiryModule,
+    adminAccountModule: AdminAccountModule
   },
   plugins: [
     createPersistedState({
