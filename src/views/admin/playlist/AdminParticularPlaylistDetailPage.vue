d8080c72f514b6dd9cc7cb9f504d7dfe4e9a8dde
Hakgyu Kim <zage4448@gmail.com>
2023-09-14 12:03:07 +0900

    feat: [김학규] 관리자 특정 플레이리스트 관리 페이지 틀 완성 [MUSE-V-39]

---
+++ src/views/admin/playlist/AdminParticularPlaylistDetailPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <AdminParticularPlaylistDetailForm/>
+  </div>
+</template>
+
+<script>
+import AdminParticularPlaylistDetailForm from "@/components/admin/playlist/AdminParticularPlaylistDetailForm.vue"
+
+export default {
+  components: {
+    AdminParticularPlaylistDetailForm
+  }
+}
+</script>
+
+<style>
+    
+</style>
