b68ba0405866b6ea6172d4641c1af366c5470afb
hanhwack <hanhwack93@gmail.com>
2023-09-14 14:33:06 +0900

    feat: [한확] 관리자는 신고가 들어온 항목들을 리스트로 볼 수 있다 [MUSE-V-37]

--- src/views/admin/report/AdminOverallReportsPage.vue
+++ src/views/admin/report/AdminOverallReportsPage.vue
@@ -1,17 +1,30 @@
 <template>
     <div>
-       <AdminOverallReportsForm/>   
+       <AdminOverallReportsForm :reportList="reportList"/>   
     </div>
   </template>
   
   <script>
   import AdminOverallReportsForm from "@/components/admin/report/AdminOverallReportsForm.vue"
+  import { mapActions, mapState } from "vuex";
+
+  const reportModule = "reportModule";
   
   export default {
     components: {
       AdminOverallReportsForm
-    }
-  }
+    },
+    methods: {
+    ...mapActions(reportModule, ["requestReportListToSpring"]),
+  },
+  computed: {
+    ...mapState(reportModule, ["reportList"]),
+  },
+  async mounted() {
+    await this.requestReportListToSpring();
+  },
+};
+  
   </script>
   
   <style>
