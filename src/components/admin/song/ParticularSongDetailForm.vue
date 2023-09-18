33b107e540f3494223c62bf1573cd11d4fbed29a
Hakgyu Kim <zage4448@gmail.com>
2023-09-18 10:36:50 +0900

    feat: [김학규] 관리자는 특정 노래의 정보를 확인/수정할 수 있는 form 틀 완성 [MUSE-V-40]

--- src/components/admin/song/ParticularSongDetailForm.vue
+++ src/components/admin/song/ParticularSongDetailForm.vue
@@ -1,31 +1,146 @@
 <template>
   <div style="display: flex; justify-content: space-between;">
     <div class="d-flex">
-      <div style="background-color: black; color: white; height: 200px; width: 300px">이미지</div>
-      <div>
-        <div>제목</div>
-        <div>가수</div>
-        <div>등록일자</div>
-        <div>링크</div>
+      <div class="admin-song-img-wrapper">
+        <v-img class="mx-auto" height="200" :src="getImage(song.link)"> </v-img>
+      </div>
+      <div style="margin: 8px">
+        <div>{{ song.title }}</div>
+        <div>{{ song.artist }}</div>
+        <div>{{ song.createDate }}</div>
+        <a :href="song.link" target="_blank" style="color: white;">{{ song.link }}</a>
       </div>
     </div>
     <div>
-      가사 칸
+      <div class="lyrics" align="center" v-html="song.lyrics" v-if="!isLyricModify"></div>
+      <div v-else>
+        <v-textarea 
+          class="lyric-modify"
+          v-model="modifiedLyrics"
+          >
+        </v-textarea>
+      </div>
     </div>
     <div align="end">
-      <div><v-btn>상태 설정</v-btn></div>
-      <div><v-btn>가사 수정</v-btn></div>
-      <div><v-btn>삭제</v-btn></div>
+      <div><v-btn class="particular-song-btn">상태 설정</v-btn></div>
+      <div v-if="!isLyricModify">
+        <v-btn 
+          class="particular-song-btn" 
+          @click="isLyricModify = !isLyricModify">
+          가사 수정
+        </v-btn>
+      </div>
+      <div v-if="isLyricModify">
+        <v-btn 
+          class="particular-song-btn" 
+          @click="cancelModify">
+          수정 취소
+        </v-btn>
+      </div>
+      <div v-if="isLyricModify">
+        <v-btn 
+          class="particular-song-btn" 
+          @click="modifyLyrics">
+          수정 완료
+        </v-btn>
+      </div>
+      <div><v-btn class="particular-song-btn">삭제</v-btn></div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-    
+  data() {
+    return {
+      song: {
+        lyrics: "asdfasdf asdfadsfasdf asdfasdfasdf<br> asdfasdf <br> asdfasdf <br> asdfasdf <br> asdfasdf <br> asdfasdf <br> asdfasdf <br> asdfasdf <br> asdfasdf <br> asdfasdf <br> asdfasdf <br> ",
+        title: "제목 1",
+        artist: "가수 1",
+        link: "https://www.youtube.com/watch?v=orJSJGHjBLI"
+      },
+      isLyricModify: false,
+      modifiedLyrics: ""
+    }
+  },
+  methods: {
+    cancelModify() {
+      this.modifiedLyrics = this.song.lyrics
+      this.isLyricModify = false
+    },
+    getImage(link) {
+      return (
+        "https://img.youtube.com/vi/" +
+        link.substring(link.lastIndexOf("=") + 1) +
+        "/mqdefault.jpg"
+      );
+    },
+  },
+  watch: {
+    isLyricModify(newVal) {
+      if (newVal) {
+        const convertedLyrics = this.song.lyrics.replace(/<br\s*\/?>/g, '\n')
+        this.modifiedLyrics = convertedLyrics
+      }
+    },
+  },
 }
 </script>
 
-<style>
-    
+<style scoped>
+.particular-song-btn{
+  background-color: #5F6871;
+  width: 100px;
+  margin: 8px;
+}
+
+.admin-song-img-wrapper{
+  height: 200px; 
+  width: 300px; 
+  background-color: black;
+}
+
+.lyrics {
+  padding-right: 15px;
+  color: white;
+  width: 250px;
+  height: 200px !important;
+  overflow: scroll;
+}
+
+.lyric-modify{
+  width: 250px;
+}
+
+a:visited {
+    color: white;
+}
+
+.lyrics::-webkit-scrollbar {
+  width: 8px;
+}
+
+.lyrics::-webkit-scrollbar-track {
+  display: none;
+}
+
+.lyrics::-webkit-scrollbar-corner {
+  display: none;
+}
+
+.lyrics::-webkit-scrollbar-horizontal {
+  display: none;
+}
+
+.lyrics:not(:hover)::-webkit-scrollbar-thumb {
+  background: transparent;
+}
+
+.lyrics:hover::-webkit-scrollbar-thumb {
+  background: #888;
+}
+
+.lyrics::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
 </style>
