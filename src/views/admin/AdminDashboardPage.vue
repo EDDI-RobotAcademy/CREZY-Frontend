433a3a56498d2b0b2c450b26612dc02f6fa897b1
Hakgyu Kim <zage4448@gmail.com>
2023-09-20 14:00:30 +0900

    update: [김학규] 대시보드, 메인 페이지 분리 [MUSE-V-27]

---
+++ src/views/admin/AdminDashboardPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <AdminDashboardForm/>
+  </div>
+</template>
+
+<script>
+import AdminDashboardForm from "@/components/admin/AdminDashboardForm.vue"
+
+export default {
+  components: {
+    AdminDashboardForm
+  }
+    
+}
+</script>
+
+<style>
+    
+</style>
