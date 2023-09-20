d75655914400fda24b00b722e9512dc2b8505836
jinho1593 <50562662+jinho1593@users.noreply.github.com>
2023-09-20 15:43:35 +0900

    feat: [김진호] 사용자는 BLOCK 당한 노래를 수정할 수 있습니다 [MUSE-V-56]

---
+++ src/components/song/SongModifyForm.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <v-form @submit.prevent="onSubmit" ref="form">
+      <v-text-field type="text" class="input-field" v-model="title" placeholder="노래 제목"></v-text-field>
+      <v-text-field type="text" class="input-field" v-model="singer" placeholder="가수"></v-text-field>
+      <v-text-field type="text" class="input-field" v-model="link" placeholder="링크"></v-text-field>
+    </v-form>
+    <div class="add-button">
+      <v-btn class="mr-2" @click="onSubmit" rounded>수정</v-btn>
+      <v-btn @click="cancel" rounded>취소</v-btn>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    song: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      title: '',
+      singer: '',
+      link: ''
+    }
+  },
+  mounted() { 
+    this.title = this.song.title;
+    this.singer = this.song.singer;
+    this.link = this.song.link;
+  },
+  methods: {
+    cancel() {
+      this.$emit("cancelModify");
+    },
+    onSubmit() {
+      this.$emit("cancelModify");
+      // let songInfo = {
+      //   title: this.title,
+      //   singer: this.singer,
+      //   link: this.link
+      // }
+      // this.$emit("submit", songInfo);
+      alert('만드는중입니다!');
+    }
+  },
+}
+</script>
+<style>
+.input-field {
+  color: white;
+  height: 75px;
+  margin-top: 10px;
+}
+
+.add-button {
+  margin-top: 20px;
+  float: right;
+}
+</style>
