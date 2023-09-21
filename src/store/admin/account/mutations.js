7532c824ac13282b1a50112bc1cc5b71cc5dcd1d
YeoulCho <cyu5733@gmail.com>
2023-09-22 00:24:29 +0900

    feat: [조여울] 관리자는 계정관리 페이지에서 계정을 검색할 수 있습니다. [MUSE-V-71]

--- src/store/admin/account/mutations.js
+++ src/store/admin/account/mutations.js
@@ -1,31 +1,27 @@
 import {
-    REQUEST_ACCOUNTS_STATUS_TO_SPRING,
-    REQUEST_ACCOUNT_LIST_FOR_ADMIN_TO_SPRING,
-    REQUEST_ACCOUNT_LIST_TOTAL_TO_SPRING,
-    REQUEST_ACCOUNT_INFO_FOR_ADMIN_TO_SPRING,
-    REQUEST_CATEGORY_ACCOUNT_LIST_TO_SPRING,
-    REQUEST_ACCOUNT_WARNINGS_FOR_ADMIN_TO_SPRING
-  } from "./mutation-types";
-  
-  export default {
-    [REQUEST_ACCOUNTS_STATUS_TO_SPRING](state, receivedData) {
-      state.accountsStatus = receivedData;
-    },
-    [REQUEST_ACCOUNT_LIST_FOR_ADMIN_TO_SPRING] (state, receivedData) {
-      state.accountList = receivedData
-    },
-    [REQUEST_ACCOUNT_LIST_TOTAL_TO_SPRING] (state, receivedData) {
-      state.accountListCount = receivedData
-    },
-    [REQUEST_ACCOUNT_INFO_FOR_ADMIN_TO_SPRING] (state, receivedData) {
-      state.accountInfo = receivedData
-    },
-    [REQUEST_CATEGORY_ACCOUNT_LIST_TO_SPRING] (state, receivedData) {
-      state.accountList = receivedData.content
-      state.accountListCount = receivedData.totalElements
-    },
-    [REQUEST_ACCOUNT_WARNINGS_FOR_ADMIN_TO_SPRING] (state, receivedData) {
-      state.warnings = receivedData
-    }
-  };
-  
+  REQUEST_ACCOUNTS_STATUS_TO_SPRING,
+  REQUEST_ACCOUNT_LIST_FOR_ADMIN_TO_SPRING,
+  REQUEST_ACCOUNT_INFO_FOR_ADMIN_TO_SPRING,
+  REQUEST_CATEGORY_ACCOUNT_LIST_TO_SPRING,
+  REQUEST_ACCOUNT_WARNINGS_FOR_ADMIN_TO_SPRING
+} from "./mutation-types";
+
+export default {
+  [REQUEST_ACCOUNTS_STATUS_TO_SPRING](state, receivedData) {
+    state.accountsStatus = receivedData;
+  },
+  [REQUEST_ACCOUNT_LIST_FOR_ADMIN_TO_SPRING](state, receivedData) {
+    state.accountList = receivedData.content
+    state.accountListCount = receivedData.totalPages
+  },
+  [REQUEST_ACCOUNT_INFO_FOR_ADMIN_TO_SPRING](state, receivedData) {
+    state.accountInfo = receivedData
+  },
+  [REQUEST_CATEGORY_ACCOUNT_LIST_TO_SPRING](state, receivedData) {
+    state.accountList = receivedData.content
+    state.accountListCount = receivedData.totalPages
+  },
+  [REQUEST_ACCOUNT_WARNINGS_FOR_ADMIN_TO_SPRING](state, receivedData) {
+    state.warnings = receivedData
+  }
+};
