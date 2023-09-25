c698a5f59c7c51693fd0cb01af0a962371a540e7
hanhwack <hanhwack93@gmail.com>
2023-09-25 14:29:45 +0900

    rename: [한확] 관리자 신고 관리 도메인 분리 [MUSE-V-78]

--- src/store/report/ReportModule.js
+++ src/store/report/ReportModule.js
@@ -1,12 +1,9 @@
-import mutations from "@/store/report/mutations"
 import actions from "@/store/report/actions"
-import state from "@/store/report/states"
+
 
 const ReportModule = {
   namespaced: true,
-  state,
   actions,
-  mutations,
 }
 
 export default ReportModule
